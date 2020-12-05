c774db53f7bfef9bed8bfd8934096f607ebab2bc
Unrealf1 <fyodor.bukreev@yandex.ru>
2020-12-06 02:30:28 +0300

    it has begun

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def hello():
-    return "Hello World!"
+    if request.method == 'POST':
+        print(request)
+    else:
+        return "Hello World!"
 
+if __name__ == "__main__":
+    app.run(debug=True)
