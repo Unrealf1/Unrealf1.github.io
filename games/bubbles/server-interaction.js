d11c992dbc9ab02701d2621f6d5cc1e47513e38d
Unrealf1 <fyodor.bukreev@yandex.ru>
2021-02-02 23:08:50 +0300

    deployment fixes

--- games/bubbles/server-interaction.js
+++ games/bubbles/server-interaction.js
@@ -5,7 +5,8 @@ async function post_record(name, score, misses, is_mobile) {
         "misses": misses,
         "mobile": is_mobile
     }
-    let response = await post_data(record, "http://127.0.0.1:5000/bubbles")
+    let response = await post_data(record, "https://imagination-site.herokuapp.com/bubbles")
     let text = await response.text()
     console.log("submitted record, response is: ", text)
 }
+
