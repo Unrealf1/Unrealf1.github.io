d98272371c7abf78b5e554927b98bc8195fac7ec
Unrealf1 <fyodor.bukreev@yandex.ru>
2024-07-07 23:58:30 +0300

    Remove backend code from the site front

--- backend/commons.py
+++
@@ -1,8 +0,0 @@
-def require(json, keys):
-    for key in keys:
-        if not key in json:
-            return False
-    return True
-
-def XSS_safe(string: str):
-    return False if string.find('<') != -1 or string.find('>') != -1 else True
